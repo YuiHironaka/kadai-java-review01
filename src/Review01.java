d6007195acbf17967cf4faebe563ed7e1e62ed34
YuiHironaka <81809@DESKTOP-I27R6NU>
2024-07-27 12:53:40 +0900

    課題修正・メソッドをtaxメソッドひとつにまとめる・変数totalの作成

--- src/Review01.java
+++ src/Review01.java
@@ -3,25 +3,18 @@ public class Review01 {
 
     public static void main(String[] args) {
         int price = 1500;//料金の変数
-        double tax = 0.1;//消費税の変数
-
-        int result1 = getinclusivePrice(price, tax);
-        int result2 = gettax(price, tax);
+        int taxresult = tax(price);
+        int total = price + taxresult;
         System.out.println
-        (price+"円の商品の税込み価格は"+result1+"円"+"（消費税は"+result2+"円）です。");
+        (price+"円の商品の税込み価格は"+total+"円"+"（消費税は"+taxresult+"円）です。");
     }
 
         //税込み価格を計算
-        public static int getinclusivePrice(int price, double tax) {
-            int result1 = (int) (price*(1+tax));
-            return result1;
-        }
-
-        //消費税がいくらかを計算
-        public static int gettax(int price, double tax) {
-            int result2 = (int) (price*tax);
-            return result2;
-
+    public static int tax(int price) {
+        double taxnum1 = 0.1;//消費税の変数
+        int result = (int) (price*taxnum1);
+        return result;
     }
+
 
 }
